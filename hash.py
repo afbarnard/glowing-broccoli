76ca5757b5597ff583eeabd0186ec82c5c5b6d9e
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2017-01-08 12:02:41 -0600

    Implemented linear growth hash table.
    * Untested.  I don't yet know if it works, but the point is to write it
      "blind", without any references, and as fast as possible!
    * Time: 44:34

---
+++ hash.py
@@ -0,0 +1,88 @@
+# Hash tables, etc.
+
+
+
+def universal_hashing(key, prime=0):
+    return key
+
+
+class LinearGrowthHashTable:
+    """Linear growth hash table as used in DBs."""
+
+    def __init__(self, load_factor=0.5, bucket_capacity=2):
+        self._load_factor = load_factor
+        self._bucket_capacity = bucket_capacity
+        self._n_items = 0
+        self._key_width = 1
+        # List of buckets of items
+        self._buckets = [[]]
+
+    def __len__(self):
+        return self._n_items
+
+    def _bucket_index(self, item):
+        # Get the hashed key
+        key = hash(item)
+        #hkey = universal_hashing(key)
+        # Mask off lower order bits to find bucket, slighly different than mod?
+        bucket_idx = int((1 <<< self._key_width) & key)
+        # Strip the highest bit to bring the index into range if necessary
+        if bucket_idx > len(self._buckets):
+            bucket_idx -= 2 ** self._key_width
+        return bucket_idx
+
+    def _lookup(self, item):
+        """Return the indices (bucket, item) of the item, or just the
+        indices of its bucket (bucket, None) if not found.
+        """
+        bucket_idx = self._bucket_index(item)
+        # Find the item in this bucket
+        bucket = self._buckets[bucket_idx]
+        for item_idx in range(len(bucket)):
+            if bucket[item_idx] == item:
+                return (bucket_idx, item_idx)
+        return (bucket_idx, None)
+
+    def __contains__(self, item):
+        bucket_idx, item_idx = self._lookup(item)
+        return item_idx is not None
+
+    def add(self, item):
+        bucket_idx, item_idx = self._lookup(item)
+        if item_idx is None:
+            self._buckets[bucket_idx].append(item)
+            self._n_items += 1
+        # Grow capacity if needed
+        load = self._n_items / (len(self._buckets) * self._bucket_capacity)
+        if load > self._load_factor:
+            self._grow(bucket_index)
+
+    def del(self, item):
+        bucket_idx, item_idx = self._lookup(item)
+        if item_idx is None:
+            del self._buckets[bucket_idx][item_idx]
+            self._n_items -= 1
+
+    def _grow(self, bucket_idx):
+        """Split the given bucket into the given one and a new one."""
+        # Create buckets to split the items into
+        lo = []
+        hi = []
+        lo_idx = bucket_idx
+        hi_idx = len(self._buckets)
+        # Update the key width if needed
+        if hi_idx > 2 ** self._key_width:
+            self._key_width += 1
+        # Split the items into the new buckets
+        bucket = self._buckets[bucket_idx]
+        for item in bucket:
+            bucket_idx = self._bucket_index(item)
+            if bucket_idx == lo_idx:
+                lo.append(item)
+            elif bucket_idx == hi_idx:
+                hi.append(item)
+            else:
+                ValueError('Hashing Failure: Invalid bucket index: {}'.format(bucket_idx))
+        # Add the new buckets
+        self._buckets[lo_idx] = lo
+        self._buckets.append(hi)
