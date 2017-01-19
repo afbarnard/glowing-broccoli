f0ca3a593ced286421815dfb5f68a6ecf4bec88b
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2017-01-18 23:55:32 -0600

    Wrote sort tests.

--- src/tests.py
+++ src/tests.py
@@ -245,3 +245,122 @@ class HeapTest(unittest.TestCase):
         middle = (KEY_RANGE[0] + KEY_RANGE[-1]) // 2
         max = KEY_RANGE[-1]
         self._check_update(heap, middle, -middle, max, max)
+
+
+class SortTest(unittest.TestCase):
+
+    def _new(self):
+        return sorted
+
+    def test_empty(self):
+        sort = self._new()
+        self.assertEqual([], sort([]))
+
+    def test_sort_1(self):
+        sort = self._new()
+        things = [6]
+        answer = [6]
+        result = sort(things)
+        self.assertEqual(answer, result)
+
+    def test_sort_2(self):
+        sort = self._new()
+        things = [5, 7]
+        answer = [5, 7]
+        result = sort(things)
+        self.assertEqual(answer, result)
+        sort = self._new()
+        things = [8, 1]
+        answer = [1, 8]
+        result = sort(things)
+        self.assertEqual(answer, result)
+
+    def test_sort_11(self):
+        sort = self._new()
+        things = [2, 1, 6, 8, 2, 7, 3, 6, 1, 3, 0]
+        answer = [0, 1, 1, 2, 2, 3, 3, 6, 6, 7, 8]
+        result = sort(things)
+        self.assertEqual(answer, result)
+
+    def test_increasing(self):
+        sort = self._new()
+        things = [1, 1, 1, 1, 2, 2, 3, 3, 5, 6, 6, 6, 8]
+        answer = [1, 1, 1, 1, 2, 2, 3, 3, 5, 6, 6, 6, 8]
+        result = sort(things)
+        self.assertEqual(answer, result)
+
+    def test_decreasing(self):
+        sort = self._new()
+        things = [7, 6, 6, 5, 2, 1, 1, 0]
+        answer = [0, 1, 1, 2, 5, 6, 6, 7]
+        result = sort(things)
+        self.assertEqual(answer, result)
+
+    def test_all_equal(self):
+        sort = self._new()
+        things = [7] * 7
+        answer = [7] * 7
+        result = sort(things)
+        self.assertEqual(answer, result)
+
+    def test_with_keys(self):
+        sort = self._new()
+        things = [
+            (4, 7), (7, 0), (0, 5), (1, 5), (5, 3),
+            (8, 6), (3, 3), (9, 8), (6, 7), (2, 5),
+        ]
+        answer = [
+            (0, 5), (1, 5), (2, 5), (3, 3), (4, 7),
+            (5, 3), (6, 7), (7, 0), (8, 6), (9, 8),
+        ]
+        result = sort(things, key=lambda x: x[0])
+        self.assertEqual(answer, result)
+        things = [
+            (8, 0), (7, 1), (2, 2), (2, 3), (6, 4),
+            (3, 5), (7, 6), (5, 7), (6, 8), (7, 9),
+        ]
+        answer = [
+            (7, 9), (6, 8), (5, 7), (7, 6), (3, 5),
+            (6, 4), (2, 3), (2, 2), (7, 1), (8, 0),
+        ]
+        result = sort(things, key=lambda x: -11.1 * x[1])
+        self.assertEqual(answer, result)
+
+    def test_in_place(self):
+        sort = self._new()
+        bef = [4, 1, 4, 2, 5, 7, 7]
+        ans = [1, 2, 4, 4, 5, 7, 7]
+        aft = sort(bef)
+        self.assertEqual(ans, aft)
+        if sort is not sorted:
+            self.assertIs(bef, aft)
+
+    def test_instantiate_iterable(self):
+        sort = self._new()
+        things = range(10)
+        answer = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        result = sort(things)
+        self.assertEqual(answer, result)
+
+    def test_immutable_indexable(self):
+        sort = self._new()
+        things = (4, 8, 0, 7, 2, 4, 2)
+        answer = [0, 2, 2, 4, 4, 7, 8]
+        result = sort(things)
+        self.assertEqual(answer, result)
+
+
+class StableSortTest(SortTest):
+
+    def test_stability(self):
+        sort = self._new()
+        things = [
+            (2, 5), (0, 6), (0, 0), (0, 4), (4, 4),
+            (4, 9), (3, 1), (3, 2), (2, 8), (2, 3),
+        ]
+        answer = [
+            (0, 6), (0, 0), (0, 4), (2, 5), (2, 8),
+            (2, 3), (3, 1), (3, 2), (4, 4), (4, 9),
+        ]
+        result = sort(things, key=lambda x: x[0])
+        self.assertEqual(answer, result)
