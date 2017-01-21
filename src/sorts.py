e9710366cbf1e88100f1b1ef394404872f82270d
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2017-01-21 12:16:15 -0600

    Tested and fixed quicksort implementation.
    * Reinstated returning the pivot index from the `pick_pivot` functions.
    * Moved the middle index condition ahead of the first index condition to
      prefer an index in the middle if possible in `_pivot_median_3`.
    * Renamed `pivot_func` to `pick_pivot`.
    * Guarded `items` so that it is only accessed if it exists.
    * Changed `lo_idx` and `hi_idx` advancement to while loops for "faster"
      finding of out-of-order keys.  Added consequent early loop continue
      condition.
    * Fixed conceptual error in dealing with the empty slot so that it is
      properly kept in the middle between the low and high indices.
    * Reinstated placing of pivot after fixing empty slot conceptual error.
    * Fixed bug where `pick_pivot` was not forwarded to recursive calls of
      `_quick`.  Dropped default `pick_pivot` from `_quick` as a result.
    * Changed "index" to "item" in attribute checks.
    * Fixed renaming of `_quicksort` to `_quick` where it was forgotten.
    * Added unit tests for all pivot functions.

--- src/sorts.py
+++ src/sorts.py
@@ -2,6 +2,8 @@
 
 import random
 
+from . import tests
+
 
 def _swap(items, idx1, idx2):
     swap = items[idx1]
@@ -9,7 +11,7 @@ def _swap(items, idx1, idx2):
     items[idx2] = swap
 
 def _pivot_first(keys, start_idx, stop_idx):
-    return keys[start_idx]
+    return start_idx, keys[start_idx]
 
 def _pivot_median_3(keys, start_idx, stop_idx):
     # Get the first, middle, and last keys
@@ -18,19 +20,19 @@ def _pivot_median_3(keys, start_idx, stop_idx):
     key_middle = keys[middle_idx]
     last_idx = stop_idx - 1
     key_last = keys[last_idx]
-    # Find the median
-    if key_middle <= key_first <= key_last or key_last <= key_first <= key_middle:
-        return key_first
-    elif key_first <= key_middle <= key_last or key_last <= key_middle <= key_first:
-        return key_middle
+    # Find the median.  Prefer the middle index other things being equal.
+    if key_first <= key_middle <= key_last or key_last <= key_middle <= key_first:
+        return middle_idx, key_middle
+    elif key_middle <= key_first <= key_last or key_last <= key_first <= key_middle:
+        return start_idx, key_first
     else:
-        return key_last
+        return last_idx, key_last
 
 def _pivot_random(keys, start_idx, stop_idx):
     idx = random.randrange(start_idx, stop_idx)
-    return keys[idx]
+    return idx, keys[idx]
 
-def _quick(keys, items, start_idx, stop_idx, pivot_func=_pivot_first):
+def _quick(keys, items, start_idx, stop_idx, pick_pivot):
     length = stop_idx - start_idx
     last_idx = stop_idx - 1
     # Base case 1: List of length 1 is already sorted
@@ -48,40 +50,90 @@ def _quick(keys, items, start_idx, stop_idx, pivot_func=_pivot_first):
             return
     # List is at least length 3 which is enough to actually do quicksort
     # Pick a pivot
-    pivot_key = pivot_func(keys, start_idx, stop_idx)
+    pivot_idx, pivot_key = pick_pivot(keys, start_idx, stop_idx)
+    if items is not None:
+        pivot_item = items[pivot_idx]
     # Partition items based on the pivot.  Items equal to the pivot go
-    # in the low partition.
+    # in the low partition.  Invariant: lo_idx <= empty_idx <= hi_idx.
+    empty_idx = pivot_idx
     lo_idx = start_idx
     hi_idx = last_idx
     while lo_idx < hi_idx:
-        if keys[lo_idx] <= pivot_key:
+        # Leave all keys less than or equal to the pivot in place
+        while keys[lo_idx] <= pivot_key and lo_idx < empty_idx:
             lo_idx += 1
-        elif keys[hi_idx] > pivot_key:
+        # Leave all keys greater than the pivot in place
+        while keys[hi_idx] > pivot_key and hi_idx > empty_idx:
             hi_idx -= 1
+        # Done.  Everything was already on the correct sides of the pivot.
+        if lo_idx == empty_idx == hi_idx:
+            continue
+        # Move the empty slot to keep it in the middle
+        elif lo_idx == empty_idx or hi_idx == empty_idx:
+            # Put the empty slot in the middle unless hi_idx - lo_idx ==
+            # 1 in which case the empty and the non-empty slots need to
+            # be swapped (because if the non-empty key was already on
+            # the correct side of the pivot then lo_idx == empty_idx ==
+            # hi_idx and the case above would have been taken)
+            middle_idx = (lo_idx + hi_idx) // 2
+            if lo_idx == empty_idx and hi_idx - lo_idx == 1:
+                # Change the middle from lo_idx to hi_idx so that a swap
+                # takes place
+                middle_idx = hi_idx
+            # "Swap" empty and middle
+            keys[empty_idx] = keys[middle_idx]
+            if items is not None:
+                items[empty_idx] = items[middle_idx]
+            empty_idx = middle_idx
+        # Swap lo_idx and hi_idx as they index out-of-order items
         else:
-            # lo_idx and hi_idx now index out-of-order items.  Swap them.
             _swap(keys, lo_idx, hi_idx)
             if items is not None:
                 _swap(items, lo_idx, hi_idx)
             lo_idx += 1
             hi_idx -= 1
-    # The pivot is now placed in lo_idx (or before if equal keys)
+    # Place the pivot item in the empty slot
+    keys[empty_idx] = pivot_key
+    if items is not None:
+        items[empty_idx] = pivot_item
     # Recur on partitions (but only if non-empty -- it's possible the
     # pivot was first or last)
-    if lo_idx - 1 > start_idx:
-        _quicksort(keys, items, start_idx, lo_idx - 1)
-    if stop_idx > lo_idx + 1:
-        _quicksort(keys, items, lo_idx + 1, stop_idx)
+    if empty_idx - 1 > start_idx:
+        _quick(keys, items, start_idx, empty_idx, pick_pivot)
+    if stop_idx > empty_idx + 2:
+        _quick(keys, items, empty_idx + 1, stop_idx, pick_pivot)
 
-def quick(items, key=None, pivot_func=_pivot_median_3):
+def quick(items, key=None, pick_pivot=_pivot_median_3):
     # Instantiate items as a list if not indexable
-    if not hasattr(items, '__setindex__') or not hasattr(items, '__getindex__'):
+    if not hasattr(items, '__setitem__') or not hasattr(items, '__getitem__'):
         items = list(items)
     # Items are also keys
     if key is None:
-        _quicksort(items, None, 0, len(items), pivot_func=pivot_func)
+        _quick(items, None, 0, len(items), pick_pivot=pick_pivot)
     # Make keys
     else:
         keys = [key(item) for item in items]
-        _quicksort(keys, items, 0, len(items), pivot_func=pivot_func)
+        _quick(keys, items, 0, len(items), pick_pivot=pick_pivot)
     return items
+
+
+class QuickSortTest_PivotFirst(tests.SortTest):
+
+    def _new(self):
+        def sort(items, key=None):
+            return quick(items, key, pick_pivot=_pivot_first)
+        return sort
+
+class QuickSortTest_PivotMedian3(tests.SortTest):
+
+    def _new(self):
+        def sort(items, key=None):
+            return quick(items, key, pick_pivot=_pivot_median_3)
+        return sort
+
+class QuickSortTest_PivotRandom(tests.SortTest):
+
+    def _new(self):
+        def sort(items, key=None):
+            return quick(items, key, pick_pivot=_pivot_random)
+        return sort
