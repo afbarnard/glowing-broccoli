852136b9df041042b41957142307937b5779bf7e
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2017-01-21 13:42:16 -0600

    Implemented and tested textbook partitioning function.
    * Implemented partition function from Cormen et al's Introduction to
      Algorithms.
    * Added unit test for it.
    * Renamed `_partition` to `_partition_shell` to differentiate it.
    * Revised `_partition_shell` to put the pivot at the end and eliminate
      maintaining an empty slot.  It is now much simpler.  Adjusted the
      invariant and conditionals to match.
    * Guarded to prevent unnecessary swaps.
    * Removed superfluous `pivot_key` from pivot-picking and partitioning
      functions.

--- src/sort.py
+++ src/sort.py
@@ -11,7 +11,7 @@ def _swap(items, idx1, idx2):
     items[idx2] = swap
 
 def _pivot_first(keys, start_idx, stop_idx):
-    return start_idx, keys[start_idx]
+    return start_idx
 
 def _pivot_median_3(keys, start_idx, stop_idx):
     # Get the first, middle, and last keys
@@ -22,52 +22,40 @@ def _pivot_median_3(keys, start_idx, stop_idx):
     key_last = keys[last_idx]
     # Find the median.  Prefer the middle index other things being equal.
     if key_first <= key_middle <= key_last or key_last <= key_middle <= key_first:
-        return middle_idx, key_middle
+        return middle_idx
     elif key_middle <= key_first <= key_last or key_last <= key_first <= key_middle:
-        return start_idx, key_first
+        return start_idx
     else:
-        return last_idx, key_last
+        return last_idx
 
 def _pivot_random(keys, start_idx, stop_idx):
     idx = random.randrange(start_idx, stop_idx)
-    return idx, keys[idx]
+    return idx
 
-def _partition(keys, items, start_idx, stop_idx, pivot_idx, pivot_key):
+def _partition_shell(keys, items, start_idx, stop_idx, pivot_idx):
     """Shellsort-like partition function as I remember learning it."""
-    # Save off the pivot item so that there is an empty slot
-    pivot_item = items[pivot_idx] if items is not None else None
+    # Put the pivot at the end
+    last_idx = stop_idx - 1
+    pivot_key = keys[pivot_idx]
+    if pivot_idx != last_idx:
+        _swap(keys, pivot_idx, last_idx)
+        if items is not None:
+            _swap(items, pivot_idx, last_idx)
     # Partition items based on the pivot.  Items equal to the pivot go
-    # in the low partition.  Invariant: lo_idx <= empty_idx <= hi_idx.
-    empty_idx = pivot_idx
+    # in the low partition.  Invariant: lo_idx <= hi_idx.
     lo_idx = start_idx
-    hi_idx = stop_idx - 1
-    while lo_idx < hi_idx:
+    hi_idx = last_idx - 1
+    while lo_idx <= hi_idx:
         # Leave all keys less than or equal to the pivot in place
-        while keys[lo_idx] <= pivot_key and lo_idx < empty_idx:
+        while keys[lo_idx] <= pivot_key and lo_idx < last_idx:
             lo_idx += 1
         # Leave all keys greater than the pivot in place
-        while keys[hi_idx] > pivot_key and hi_idx > empty_idx:
+        while keys[hi_idx] > pivot_key and hi_idx > start_idx:
             hi_idx -= 1
-        # Done.  Everything is now on the correct sides of the pivot.
-        if lo_idx == empty_idx == hi_idx:
-            continue
-        # Move the empty slot to keep it in the middle
-        elif lo_idx == empty_idx or hi_idx == empty_idx:
-            # Put the empty slot in the middle unless hi_idx - lo_idx ==
-            # 1 in which case the empty and the non-empty slots need to
-            # be swapped (because if the non-empty key was already on
-            # the correct side of the pivot then lo_idx == empty_idx ==
-            # hi_idx and the case above would have been taken)
-            middle_idx = (lo_idx + hi_idx) // 2
-            if lo_idx == empty_idx and hi_idx - lo_idx == 1:
-                # Change the middle from lo_idx to hi_idx so that a swap
-                # takes place
-                middle_idx = hi_idx
-            # "Swap" empty and middle
-            keys[empty_idx] = keys[middle_idx]
-            if items is not None:
-                items[empty_idx] = items[middle_idx]
-            empty_idx = middle_idx
+        # Exit early if everything is now on the correct sides of the
+        # pivot
+        if lo_idx >= hi_idx:
+            break
         # Swap lo_idx and hi_idx as they index out-of-order items
         else:
             _swap(keys, lo_idx, hi_idx)
@@ -75,12 +63,50 @@ def _partition(keys, items, start_idx, stop_idx, pivot_idx, pivot_key):
                 _swap(items, lo_idx, hi_idx)
             lo_idx += 1
             hi_idx -= 1
-    # Place the pivot item in the empty slot
-    keys[empty_idx] = pivot_key
-    if items is not None:
-        items[empty_idx] = pivot_item
+    # Place the pivot item in its place between the low and high
+    # partitions by swapping the last item (the pivot) with the bottom
+    # high item
+    if lo_idx != last_idx:
+        _swap(keys, lo_idx, last_idx)
+        if items is not None:
+            _swap(items, lo_idx, last_idx)
     # Return the final index of the pivot
-    return empty_idx
+    return lo_idx
+
+def _partition_clrsia(keys, items, start_idx, stop_idx, pivot_idx):
+    """The partition function from the description of quicksort in Cormen,
+    Leiserson, Rivest, and Stein's Introduction to Algorithms.
+
+    """
+    # Put the pivot at the end
+    last_idx = stop_idx - 1
+    pivot_key = keys[pivot_idx]
+    if pivot_idx != last_idx:
+        _swap(keys, pivot_idx, last_idx)
+        if items is not None:
+            _swap(items, pivot_idx, last_idx)
+    # Partition items based on the pivot.  Items equal to the pivot go
+    # in the low partition.  Invariants: low parition:
+    # [start_idx:lo_end], high parition: [lo_end:hi_end], start_idx <=
+    # lo_end <= hi_end <= last_idx.
+    lo_end = start_idx
+    for hi_end in range(start_idx, last_idx):
+        # Put a low item into place by swapping it with the bottom high
+        # item
+        if keys[hi_end] <= pivot_key:
+            _swap(keys, lo_end, hi_end)
+            if items is not None:
+                _swap(items, lo_end, hi_end)
+            lo_end += 1
+    # Place the pivot item in its place between the low and high
+    # partitions by swapping the last item (the pivot) with the bottom
+    # high item
+    if lo_end != last_idx:
+        _swap(keys, lo_end, last_idx)
+        if items is not None:
+            _swap(items, lo_end, last_idx)
+    # Return the final index of the pivot
+    return lo_end
 
 def _quick(keys, items, start_idx, stop_idx, pick_pivot, partition):
     length = stop_idx - start_idx
@@ -100,9 +126,9 @@ def _quick(keys, items, start_idx, stop_idx, pick_pivot, partition):
             return
     # List is at least length 3 which is enough to actually do quicksort
     # Pick a pivot
-    pivot_idx, pivot_key = pick_pivot(keys, start_idx, stop_idx)
+    pivot_idx = pick_pivot(keys, start_idx, stop_idx)
     # Partition items based on the pivot
-    pivot_idx = partition(keys, items, start_idx, stop_idx, pivot_idx, pivot_key)
+    pivot_idx = partition(keys, items, start_idx, stop_idx, pivot_idx)
     # Recur on partitions, but only if at least size 2.  It's possible
     # the pivot was first or last resulting in an empty partition.
     # Plus, a partition of size 1 is already sorted.
@@ -111,7 +137,7 @@ def _quick(keys, items, start_idx, stop_idx, pick_pivot, partition):
     if stop_idx > pivot_idx + 2:
         _quick(keys, items, pivot_idx + 1, stop_idx, pick_pivot, partition)
 
-def quick(items, key=None, pick_pivot=_pivot_median_3, partition=_partition):
+def quick(items, key=None, pick_pivot=_pivot_median_3, partition=_partition_shell):
     # Instantiate items as a list if not indexable
     if not hasattr(items, '__setitem__') or not hasattr(items, '__getitem__'):
         items = list(items)
@@ -144,4 +170,11 @@ class QuickSortTest_PivotRandom(tests.SortTest):
     def _new(self):
         def sort(items, key=None):
             return quick(items, key, pick_pivot=_pivot_random)
+        return sort
+
+class QuickSortTest_PivotFirst_PartitionClrsia(tests.SortTest):
+
+    def _new(self):
+        def sort(items, key=None):
+            return quick(items, key, pick_pivot=_pivot_first, partition=_partition_clrsia)
         return sort
